58cd29eb46d809dca4dd8950cc66b590dd308c23
Olivér Kiss <oliverkiss@proton.me>
2024-06-09 15:51:53 -0400

    feat: add scraper for party vote data

---
+++ code/scrape_party_list.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import pandas as pd
+
+# SET A VALID VALASZTAS.HU TIMESTAMP HERE
+timestamp = '06092057'
+
+data = [json.loads(requests.get(f'https://vtr.valasztas.hu/onk2024/data/{timestamp}/szavossz/01/SzeredmTelep-01-0{(2-len(str(i+1)))*"0"+str(i+1)}.json').content) for i in range(23)]
+
+dt = []
+jelolt = {1:"MKKP",2:"Mi Hazank",3:"Nep Partjan",4:"Munkaspart",5:"Szolidaritas",6:"TISZA",7:"Vitezy-LMP",8:"FIDESZ",9:"DK-MSZP-PARBESZED",10:"MOMENTUM"}
+for ker in data:
+    for x in ker['data']['tevk_csoport']:
+        if x['eredmenyek'][0]['valaltip']!="F":
+            continue
+        for y in x['eredmenyek'][0]['tetelek']:
+            dt.append({"Valasztas":"Fovarosi lista","MAZ":ker['data']['maz'],"TAZ":ker['data']['taz'],"Telepules":ker['data']['telnev'],"TEVK":x['tevk'],"Lista":jelolt[y['szavlap_sorsz']],'Szavazat':y['szavazat']})
+
+pd.DataFrame(dt).to_csv('budapest_party_list.csv', index=False)
