6c1f61bf2a3a7f0583b67c90b732a08d8e0fe451
Miklós Koren <miklos.koren@gmail.com>
2024-06-09 22:45:49 +0200

    Pull data at 06092233

--- code/scrape_party_list.py
+++ code/scrape_party_list.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 
 # SET A VALID VALASZTAS.HU TIMESTAMP HERE
-timestamp = '06092057'
+timestamp = '06092233'
 
 data = [json.loads(requests.get(f'https://vtr.valasztas.hu/onk2024/data/{timestamp}/szavossz/01/SzeredmTelep-01-0{(2-len(str(i+1)))*"0"+str(i+1)}.json').content) for i in range(23)]
 
@@ -16,4 +16,4 @@ for ker in data:
         for y in x['eredmenyek'][0]['tetelek']:
             dt.append({"Valasztas":"Fovarosi lista","MAZ":ker['data']['maz'],"TAZ":ker['data']['taz'],"Telepules":ker['data']['telnev'],"TEVK":x['tevk'],"Lista":jelolt[y['szavlap_sorsz']],'Szavazat':y['szavazat']})
 
-pd.DataFrame(dt).to_csv('budapest_party_list.csv', index=False)
+pd.DataFrame(dt).to_csv('data/2024/budapest_party_list.csv', index=False)
