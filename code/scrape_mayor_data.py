51d72eb116b8f75e8e7f84c8ddfe90e705738639
Olivér Kiss <oliverkiss@proton.me>
2024-06-09 15:35:33 -0400

    feat: add scraping code for mayoral election results

---
+++ code/scrape_mayor_data.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import pandas as pd
+
+# SET A VALID VALASZTAS.HU TIMESTAMP HERE
+timestamp = '06092057'
+
+data = [json.loads(requests.get(f'https://vtr.valasztas.hu/onk2024/data/{timestamp}/szavossz/01/SzeredmTelep-01-0{(2-len(str(i+1)))*"0"+str(i+1)}.json').content) for i in range(23)]
+
+dt = []
+jelolt = {1:"Grundtner",2:"Vitezy",3:"Szentkiralyi",4:"Karacsony"}
+for ker in data:
+    for x in ker['data']['tevk_csoport']:
+        if x['eredmenyek'][0]['valaltip']!="2":
+            continue
+        for y in x['eredmenyek'][0]['tetelek']:
+            dt.append({"Valasztas":"Fopolgarmester","MAZ":ker['data']['maz'],"TAZ":ker['data']['taz'],"Telepules":ker['data']['telnev'],"TEVK":x['tevk'],"Jelolt":jelolt[y['szavlap_sorsz']],'Szavazat':y['szavazat']})
+
+pd.DataFrame(dt).to_csv('fopolgarmester.csv', index=False)
